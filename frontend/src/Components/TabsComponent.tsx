c6402506b4a829a36264292c85001d9112691e1a
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2024-04-04 20:11:54 +0530

    Update

--- frontend/src/Components/TabsComponent.tsx
+++ frontend/src/Components/TabsComponent.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import React, { useEffect, useRef, useState } from 'react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Center } from '@chakra-ui/react';
 import style from '../Css/TabsComponent.module.css'
 import { useAppSelector } from '../Redux/Store/hook';
 import { customersValueFromReduxStore } from '../Redux/CustomerReducer/reducer';
@@ -7,9 +7,9 @@ import { customersValueFromReduxStore } from '../Redux/CustomerReducer/reducer';
 const TabsComponent = () => {
 
     const customers = useAppSelector(customersValueFromReduxStore);
-    const [photoIndex, setPhotoIndex] = useState<number>(0); 
+    const [photoIndex, setPhotoIndex] = useState<number>(0);
+    const tabListRef = useRef<HTMLDivElement>(null);
 
-    
     useEffect(() => {
         const interval = setInterval(() => {
             // Increment the photo index circularly
@@ -17,51 +17,84 @@ const TabsComponent = () => {
         }, 10000); // Change photo every 10 seconds
 
         return () => clearInterval(interval); // Cleanup function to clear interval
-    }, []); 
+    }, []);
+
+    const shufflePhotos = (photos: string[]) => {
+        // Shuffle photos array
+        for (let i = photos.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [photos[i], photos[j]] = [photos[j], photos[i]];
+        }
+        return photos;
+    };
+
+    const handleScroll = () => {
+        if (tabListRef.current) {
+            const scrollTop = tabListRef.current.scrollTop;
+            const visibleItems = Math.floor(scrollTop / 40); // Assuming each item height is 40px
+            setPhotoIndex(visibleItems * 9); // Show 10 items with every scroll
+        }
+    };
+
+
     // style={{display: 'flex',}}
     // style={{border: '2px solid green', display: 'flex', flexDirection: 'column', width: '30%', textAlign: 'left'}}
     return (
         <div>
+
+            <h1 className={style.superHeroes}>Super Heroes</h1>
             <Tabs orientation='vertical' variant={'unstyled'} >
-                <TabList className={style.tablist}>
+                <TabList className={style.tablist}
+                ref={tabListRef}
+                onScroll={handleScroll}
+
+                >
 
                     {
                         customers.map((el) => (
-                            <Tab key={el.id}>
+                            <Tab key={el.id} style={{borderBottom: '1px solid grey'}} _selected={{ color: 'black', bg: '#E2E8F0', borderRight: '3px solid #4A5568' }}>
                                 <div style={{ textAlign: 'left' }}>
-                                    <h1>Customer {el.id}</h1>
-                                    <p>{el.title}</p>
+                                    <h1 className={style.heading}>{el.name}</h1>
+                                    <p>{el.title.length > 150 ? `${el.title.slice(0,150)}...` : el.title}</p>
+                                     
                                 </div>
                             </Tab>
                         ))
                     }
-                    {/* <Tab >
-                    <div style={{textAlign: 'left'}}>
-                        <h1>Customer 01</h1>
-                        <p>Lorem ipsum dolor sit amet</p>
 
-                    </div>
-                    </Tab>
-                    <Tab>Two</Tab>
-                    <Tab>Three</Tab> */}
                 </TabList>
 
-                <TabPanels className={style.tabpannels}>
+                <TabPanels>
 
                     {
                         customers.map((el) => (
-                            <TabPanel>
-                                <h1>customers details hers</h1>
-                                <p>{el.title}</p>
-                                <div>
+                            <TabPanel className={style.tabpannelCont}>
+                                <div className={`${style.customerDetail}`}>
+                                    <div>
+                                        <h1 className={style.heading}>{el.name}</h1>
+                                        <p>{el.address}</p>
+                                    </div>
+                                </div>
 
-                                    {
-                                        el.photos.map((el, index) => (
-                                            <div key={index} style={{ display: index === photoIndex ? 'block' : 'none' }}>
-                                                <img src={el} alt="" />
-                                            </div>
-                                        ))
-                                    }
+                                <div className={`${style.gridContainer}`}>
+                                    {el.title}
+                                </div>
+
+                                <div className={`${style.gridContainer}`}>
+
+                                    
+                                        <div className={`${style.tabpannels}`}>
+
+
+
+                                            {el.photos.slice(photoIndex, photoIndex + 9).map((photo, index) => (
+                                                <div key={index} className={`${style.imgCont}`} >
+                                                    <img src={photo} alt="" className={style.image} />
+                                                </div>
+                                            ))}
+                                        </div>
+                                    
+
                                 </div>
                             </TabPanel>
                         ))
--- frontend/src/Routes/Main.tsx
+++ frontend/src/Routes/Main.tsx
@@ -2,22 +2,37 @@ import React, { useEffect } from 'react'
 import TabsComponent from '../Components/TabsComponent'
 import { useAppDispatch, useAppSelector } from '../Redux/Store/hook'
 import { getAllCustomersData } from '../Redux/CustomerReducer/Function'
-import { customersValueFromReduxStore } from '../Redux/CustomerReducer/reducer'
+import { customersIsLoadingValueFromReduxStore, customersValueFromReduxStore } from '../Redux/CustomerReducer/reducer'
+import { Spinner } from '@chakra-ui/react';
+
+import style from '../Css/TabsComponent.module.css'
 
 const Main = () => {
 
-    const dispatch = useAppDispatch()
-    
-    
-    
-    useEffect(()=>{
-     dispatch(getAllCustomersData())
+  const dispatch = useAppDispatch();
+  const loading = useAppSelector(customersIsLoadingValueFromReduxStore)
 
-    }, [])
-    
+
+
+  useEffect(() => {
+    dispatch(getAllCustomersData())
+
+  }, [])
+
   return (
     <div>
-      <TabsComponent />
+      {
+        loading ? <div className={style.loading}>
+          <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl'
+          />
+        </div> : <TabsComponent />
+      }
+      
     </div>
   )
 }
